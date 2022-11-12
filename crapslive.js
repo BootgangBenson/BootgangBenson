9cbf2d2b3f6848fcdefc77e40891e97836eb531e
RLBIII <95179859+BootgangBenson@users.noreply.github.com>
2022-11-12 04:30:28 -0700

    Add files via upload

---
+++ crapslive.js
@@ -0,0 +1,25 @@
+function play()
+{
+    var die1 = Math.ceil(Math.random() * 6); 
+    var die2 = Math.ceil(Math.random() * 6); 
+    var sum = die1 + die2;
+
+
+ 
+        document.getElementById("die1Res").innerHTML = "die1 = " + die1;
+        document.getElementById("die2Res").innerHTML = "die1 = " + die2;
+        document.getElementById("sumRes").innerHTML = "sum = " + sum;
+
+        if ( sum == 7  || sum == 11)
+        {
+            document.getElementById("finalres").innerHTML = "CRAPS YOU LOSE";
+            }
+        else if (die1 == die2 && die1 % 2 == 0)
+        {
+            document.getElementById("finalres").innerHTML = "Congrats You Won!";
+        }
+        else
+        {
+            document.getElementById("finalres").innerHTML = "PUSH";
+        }
+    }
